9f4f19e7bdd0c2b475106d3ad077a6bef746b28e
Diego Torreblanca <diegotorreblanca@Macbooks-MacBook-Air.local>
2024-10-16 18:18:38 -0300

    Fix not used var

--- src/app/components/Drawer.tsx
+++ src/app/components/Drawer.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { Card, Drawer, Typography } from 'antd';
-import Link from "next/link";
 import { useDrawer } from '../context/DrawerContext';
 import { useStorage } from '../hooks/useStorage';
 import { useRouter } from 'next/navigation';
@@ -11,7 +10,7 @@ const DrawerCustom = () => {
     const { getLocalStorage, setLocalStorage } = useStorage();
     const router = useRouter();
 
-    const editName = (name: string) => {
+    const editName = () => {
         setLocalStorage('name', '');
         router.replace('/');
         toggleDrawer();
@@ -35,7 +34,7 @@ const DrawerCustom = () => {
             {existsName() && (
             <Card
                 className="mt-8 p-2 my-2 w-64 h-14 bg-gray-200 dark:bg-gray-600 rounded cursor-pointer text-center flex justify-center items-center dark:border-gray-700"
-                onClick={() => editName('')}
+                onClick={() => editName()}
             >
                 <Typography.Text className="dark:text-white">Change Name</Typography.Text>
             </Card>
